8c2a53744893d10a464f22454e3d4e8a1f21f377
PX Slip (Will K.) <dev@pxslip.com>
2021-11-12 00:11:52 +0000

    Semantically correct

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-import { createServer } from 'http';
-import dot from 'dot';
-import { hostname } from 'os';
+import { createServer } from "http";
+import dot from "dot";
+import { hostname } from "os";
 
 const template = dot.template(`
-  <!doctype html>
+<!doctype html>
+<html>
   <head>
     <title>Processed by {{=it.host}}</title>
   </head>
   <body>
     <h1>This page was created on the droplet with hostname: {{=it.host}}</h1>
   </body>
+</html>
 `);
 
 const server = createServer((req, res) => {

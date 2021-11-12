02e5476cc33cca1e548a0456c2398573302f6c9b
PX Slip (Will K.) <dev@pxslip.com>
2021-11-12 00:07:29 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import { createServer } from 'http';
+import dot from 'dot';
+import { hostname } from 'os';
+
+const template = dot.template(`
+  <!doctype html>
+  <head>
+    <title>Processed by {{=it.host}}</title>
+  </head>
+  <body>
+    <h1>This page was created on the droplet with hostname: {{=it.host}}</h1>
+  </body>
+`);
+
+const server = createServer((req, res) => {
+  const host = hostname();
+  res.end(template({ host }));
+});
+
+server.listen(3000);
